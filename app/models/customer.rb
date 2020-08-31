0a4974729b27b1c78f60e2204155b5cb4d38fc9d
varg90 <kuhn.alexey@gmail.com>
2020-08-31 15:29:00 +0700

    create order resource, add order seeds

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,18 @@
 class Customer < ApplicationRecord
   validates :email, uniqueness: true,
             format: { with: /\A[^@\s]+@[^@\s]+\z/i }
+  validates :birthdate, presence: true
+  validate :allowed_to_order_alcohol
+
+  has_many :orders, dependent: :destroy
+
+  def allowed_to_order_alcohol
+    if birthdate < 21.years.from_now
+      errors.add(:birthdate, 'Grow up and come back! Sorry')
+    end
+  end
+
+  def total_amount
+    orders.sum &:total
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_130614) do
+ActiveRecord::Schema.define(version: 2020_08_31_073526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 2020_08_28_130614) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.string "number", null: false
+    t.datetime "date", null: false
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  add_foreign_key "orders", "customers"
 end
